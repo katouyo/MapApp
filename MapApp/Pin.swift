891d977a0b360f13c14e4cd4c1675d41a5b5035c
katouyo <yothiac13@gmail.com>
2018-10-30 15:38:05 +0900

    first commit

---
+++ MapApp/Pin.swift
@@ -0,0 +1,13 @@
+//
+//  Pin.swift
+//  MapApp
+//
+//  Created by Yoshiaki Kato on 2018/10/28.
+//  Copyright © 2018 Yoshiaki Kato. All rights reserved.
+//
+
+import UIKit
+
+class Pin: NSObject {
+
+}
